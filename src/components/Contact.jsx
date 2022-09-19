847c86f1cbfe8cefe174aaa518b4ba07b0d16762
avacadok <avakuang8@gmail.com>
2022-09-18 17:45:53 -0700

    use emailJs for better performance

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,48 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { Container, Row, Col } from "react-bootstrap";
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
-  const formInfo = {
-    firstName: '',
-    lastName: '',
-    email: '',
-    phone: '',
-    message: ''
-  };
-
-  const [formDetails, setFormDetails] = useState(formInfo);
+  const form = useRef();
   const [buttonText, setButtonText] = useState('Send');
   const [status, setStatus] = useState({});
 
-  const onFormUpdate = (category, value) => {
-    setFormDetails({
-      ...formDetails,
-      [category]: value
-    })
-  }
-
-  const handleSubmit = async (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    setButtonText("Sending...");
-    let response = await fetch("https://scintillating-squirrel-4eca2b.netlify.app/contact", {
-      method: "POST",
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-      },
-      body: JSON.stringify(formDetails),
-    });
-    setButtonText("Send");
-    let result = await response.json();
-    setFormDetails(formInfo);
-    if (result.code === 200) {
-      setStatus({ success: true, message: 'Message sent successfully, thank you 💜' });
-
-    } else {
-      setStatus({ success: false, message: 'Sorry something went wrong, please try again later 😔' });
-    }
+    setButtonText("Sending");
+    emailjs.sendForm('service_ixx2u2r', 'template_cr5uj88', form.current, 'fzVeZRD9lNra-lyWB')
+      .then((result) => {
+          console.log("result",result.text);
+          setStatus({ success: true, message: 'Message sent successfully, thank you 💜' });
+          setButtonText("Send");
+      }, (error) => {
+          console.log(error.text);
+          setStatus({ success: false, message: 'Sorry something went wrong, please try again later 😔' });
+      });
+    e.target.reset();
   };
-      console.log("status",status.message)
+  
   return (
     <section className="contact-ava" id="contact">
       <Container >
@@ -60,26 +39,18 @@ const Contact = () => {
                   </p>
                 </Col>
               }
-            <form onSubmit={handleSubmit} data-netlify="true">
+            <form onSubmit={handleSubmit} ref={form} data-netlify="true">
               <Row className="contact-detail">
                 <Col md={6} className="px-1">
-                  <input type="text" value={formDetails.firstName} placeholder="First Name" onChange={(e) => onFormUpdate('firstName', e.target.value)} />
+                  <input type="text" placeholder="Name" name="from_name" />
                 </Col>
 
                 <Col md={6} className="px-1">
-                  <input type="text" value={formDetails.lastName} placeholder="Last Name" onChange={(e) => onFormUpdate('lastName', e.target.value)} />
-                </Col>
-
-                <Col md={6} className="px-1">
-                  <input type="email" value={formDetails.email} placeholder="Email" onChange={(e) => onFormUpdate('email', e.target.value)} />
-                </Col>
-
-                <Col md={6} className="px-1">
-                  <input type="text" value={formDetails.phone} placeholder="Phone Number" onChange={(e) => onFormUpdate('phone', e.target.value)} />
+                  <input type="email" placeholder="Email" name="from_email"/>
                 </Col>
 
                 <Col size={12} className="textarea">
-                  <textarea rows="6" value={formDetails.message} placeholder="Message" onChange={(e) => onFormUpdate('message', e.target.value)}></textarea>
+                  <textarea rows="6" placeholder="Message" name="message"></textarea>
                   <button type="submit"><span>{buttonText}</span></button>
                 </Col>
               </Row>
