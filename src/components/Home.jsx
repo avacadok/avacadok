ede2f9a16dd4267b05794f2e9ad3857f443af21d
avacadok <avakuang8@gmail.com>
2022-09-18 20:54:08 -0700

    remove home page fade in effect

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -50,27 +50,22 @@ const Banner = () => {
   }
   return (
     <section className="home" id="home">
-      <TrackVisibility>
-        {({ isVisible }) =>
-          <div className={isVisible ? "animate__animated animate__fadeInDown animate__slow" : ""}>
-            <Container>
-              <Row className="align-item-center">
-                <Col xs={12} md={6} xl={7}>
-                  <span className="tagline">
-                    Welcome to my Portfolio
-                  </span>
-                  <h1>Hi I am Ava ✨<br /> A <span className="wrap">{text}</span></h1>
-                  <p>I am a web developer who transitioned from the financial banking field. I am interested in web development and I've found a great love for how my code can create a story for users visiting my website. I am driven to learn new concepts and work well in highly collaborative work environment.</p>
-                </Col>
+      <Container>
+        <Row className="align-item-center">
+          <Col xs={12} md={6} xl={7}>
+            <span className="tagline">
+              Welcome to my Portfolio
+            </span>
+            <h1>Hi I am Ava ✨<br /> A <span className="wrap">{text}</span></h1>
+            <p>I am a web developer who transitioned from the financial banking field. I am interested in web development and I've found a great love for how my code can create a story for users visiting my website. I am driven to learn new concepts and work well in highly collaborative work environment.</p>
+          </Col>
 
-                <Col xs={12} md={6} xl={5}>
-                  <img className="creative-img" src="https://imgur.com/wiE1POO.gif" alt="creativity" />
-                </Col>
+          <Col xs={12} md={6} xl={5}>
+            <img className="creative-img" src="https://imgur.com/wiE1POO.gif" alt="creativity" />
+          </Col>
 
-              </Row>
-            </Container>
-          </div>}
-      </TrackVisibility>
+        </Row>
+      </Container>
     </section>
   )
 }
